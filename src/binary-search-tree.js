1a545bb2fef23c5b7fd7d32f5d129f1c91866428
Oksana-bondareva <oksanchik252@gmail.com>
2023-12-18 20:00:39 +0300

    feat: add solution task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,54 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+ const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  rootNode = null;
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
+  }
+ 
+  add(data) {
+    this.rootNode = this.addDate(this.rootNode, data);
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addDate(node, data) {
+    if (!node) {
+      return new Node(data);
+    }
+    if (node.data === data) {
+      return node;
+    }
+    if (data < node.data) {
+      node.left = this.addDate(node.left, data);
+    } else {
+      node.right = this.addDate(node.right, data);
+    }
+    return node;
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return this.searchDate(this.rootNode, data) !== null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.searchDate(this.rootNode, data);
+  }
+
+  searchDate(node, data) {
+    if (!node) {
+      return null;
+    }
+    if (node.data === data) {
+      return node;
+    }
+    return data < node.data ? 
+      this.searchDate(node.left, data) :
+      this.searchDate(node.right, data);
   }
 
   remove(/* data */) {
